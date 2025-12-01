4f0ba3d3e1fd2adccd9d45593d89611d4421b28d
Aryan Kate <aryankate2020@gmail.com>
2025-12-01 19:20:52 +0530

    updated herat button state function

--- components/media/favourite/heart-button.tsx
+++ components/media/favourite/heart-button.tsx
@@ -17,49 +17,48 @@ interface HeartButtonProps {
 
 
 
+import { useState } from "react"
 
-export function HeartButton({mediaInfo , initialFavourite , onFavoritesChange , session }  : HeartButtonProps){
-
-    
-
+export function HeartButton({ mediaInfo, initialFavourite = false, onFavoritesChange, session }: HeartButtonProps) {
+    const [favourite, setFavourite] = useState(initialFavourite)
     const { openPopup } = UsePopUp()
-    const handleFavourites = async() => {
 
-        if(!session) return openPopup('Sign in to add movies to your favourites')
-            
-        if(initialFavourite) {
-            const res = await removeFromFavourites(mediaInfo)
-            onFavoritesChange?.(res ,false)
-      
-        } else{
-  
-            const res = await addToFavourites(mediaInfo)
-            onFavoritesChange?.(res ,true)
-    
+    const handleFavourites = async () => {
+        if (!session) return openPopup('Sign in to add movies to your favourites')
+
+     
+        setFavourite(prev => !prev)
+        onFavoritesChange?.(null, !favourite) 
+
+        try {
+            if (favourite) {
+                await removeFromFavourites(mediaInfo)
+            } else {
+                await addToFavourites(mediaInfo)
+            }
+        } catch (err) {
+           
+            console.error(err)
+            setFavourite(prev => !prev)
+            onFavoritesChange?.(null, favourite)
         }
     }
 
     return (
         <>
-
-            {/* this should be a component */}
-            <SignInPopUp />
-                
-          
-
-            {/* when this is clicked */}
-            <div className="group" >
+            <SignInPopUp/>
+            <div className="group">
                 <motion.button
-                    style={{ 
-                        backdropFilter : initialFavourite ? 'blur(0px)' : 'blur(14px)',
-                        backgroundColor : initialFavourite ? '#FFFFFFE6' : 'transparent',
-                        color : initialFavourite ? '#E11D48' : '#FFFFFFE6',
-                        borderColor : initialFavourite ? '#FFFFFF4D': '' 
+                    style={{
+                        backdropFilter: favourite ? 'blur(0px)' : 'blur(14px)',
+                        backgroundColor: favourite ? '#FFFFFFE6' : 'transparent',
+                        color: favourite ? '#E11D48' : '#FFFFFFE6',
+                        borderColor: favourite ? '#FFFFFF4D' : ''
                     }}
                     onClick={handleFavourites}
-                    className=" p-3 md:p-3.5 flex items-center justify-center rounded-xs border border-white/20 cursor-pointer group-hover:scale-95 duration-200 "
-                    >
-                    <FaHeart className="text-xl md:text-2xl group-hover:scale-120 duration-200"/>
+                    className="p-3 md:p-3.5 flex items-center justify-center rounded-xs border border-white/20 cursor-pointer group-hover:scale-95 duration-200"
+                >
+                    <FaHeart className="text-xl md:text-2xl group-hover:scale-120 duration-200" />
                 </motion.button>
             </div>
         
